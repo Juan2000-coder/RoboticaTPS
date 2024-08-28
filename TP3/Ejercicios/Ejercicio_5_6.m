8c7d76230810fd250692595ffd587117307c5d2a
Juan <borquez.juan00@gmail.com>
2024-08-28 16:51:06 -0300

    robot.m datasheets

--- TP3/Ejercicios/Ejercicio_5_6.m
+++ TP3/Ejercicios/Ejercicio_5_6.m
@@ -7,38 +7,40 @@ fprintf('#                Ejercicios 5  TP3                   #\n');
 fprintf('######################################################\n\n');
 
 
-fprintf("------------Robot de la sección 3.2------------------\n\n");
+fprintf("------------Robot de la sección 3.3------------------\n\n");
 
 
 % Parametros DH robot
-DH = [0.0 450.0   75.0  pi/2  0;
-      0.0   0.0  300.0  0.0   0;
-      0.0   0.0   75.0  pi/2  0;
-      0.0 320.0    0.0 -pi/2  0;
-      0.0   0.0    0.0  pi/2  0;
-      0.0  80.0    0.0  0.0   0];
+DH = [0.0 340.0   0.0   pi/2  0;
+      0.0   0.0   0.0  -pi/2  0;
+      0.0 400.0   0.0   pi/2  0;
+      0.0   0.0   0.0  -pi/2  0;
+      0.0 400.0   0.0   pi/2  0;
+      0.0   0.0   0.0  -pi/2  0; 
+      0.0  10.0   0.0   0.0   0]; % Adopto un valor genérico para MF(Media Flange)
+
 
 % Variables articulares
-q  = [0 0 0 0 0 0];
+q  = [0.0 0.0 0.0 0.0 0.0 0.0 0.0];
 
 % Construcción del robot
-robot      = SerialLink(DH, 'name', 'FANUC Paint Mate 200iA');
+robot      = SerialLink(DH, 'name', 'LBR iiwa 7 R800');
 
 
 % Offsets iniciales
-robot.offset = [0.0 pi/2 0.0 0.0 0.0 0.0];
+robot.offset = [0.0 0.0 0.0 0.0 0.0 0.0];
 
 % Límites obtenidos del datasheet del robot
 robot.qlim   = deg2rad([-170   170;
-                        -100   100;
-                        -194   194;
-                        -190   190;
                         -120   120;
-                        -360   360]);
+                        -170   170;
+                        -120   120;
+                        -170   170;
+                        -120   120;
+                        -175   175]);
 
 
 % Plot del robot
-
 robot.plot(q, 'trail', {'r', 'LineWidth', 2});
 
 robot.teach();
