d962a1f4fce000da97ca3c30b923dea74813b40a
Juan <borquez.juan00@gmail.com>
2024-09-21 00:39:54 -0300

    Ejercicio 5

--- TP5/Ejercicios/Ejercicio_5.m
+++ TP5/Ejercicios/Ejercicio_5.m
@@ -1,81 +1,86 @@
 clc; clear all; close all;
 
 fprintf('######################################################\n');
-fprintf('#                Ejercicios 1  TP5                   #\n');
+fprintf('#                Ejercicios 5  TP5                   #\n');
 fprintf('######################################################\n\n');
 
 %% DEFINICIÓN DEL ROBOT, VARIABLES Y PLOT
-fprintf("------------Robot del ejercicio 1------------------\n\n");
+fprintf("------------Robot del ejercicio 2.1------------------\n\n");
 
-a1 = 2;
-a2 = 1;
+% Parametros DH robot
+a1 = 1;
+a2 = 2;
+a3 = 3;
 
-fprintf("\nLongitud de los eslabones:");
-fprintf("\na1 =  %f", a1);
-fprintf("\na2 =  %f", a2);
+dh = [0.0 0.0 a1 0.0 0;
+      0.0 0.0 a2 0.0 0;
+      0.0 0.0 a3 0.0 0];
 
-DH          =      [0 0 a1 0 0;
-                    0 0 a2 0 0];
-robot       = SerialLink(DH, 'name', '2R');
-robot.qlim  = [-pi pi
-               -pi pi];
-q = [0 0];
+% Variables articulares
+q  = [0 0 0];
 
+% Construcción del robot
+robot        = SerialLink(dh, 'name', 'RRR');
+
+% Offsets inniciales
+robot.offset = [0 0 0];
+
+% Límites
+robot.qlim   = [-pi pi;
+                -pi pi;
+                -pi pi];
 
 % Plot del robot
-figure(1);
-robot.plot(q, 'workspace', [-(a1 + a2)*1.2 (a1 + a2)*1.2 -(a1 + a2)*1.2 (a1 + a2)*1.2 -1 1],'scale', 0.5,'trail', {'r', 'LineWidth', 2}, 'jointdiam', 0.5);
+R = a1 + a2 + a3;
+robot.plot(q, 'workspace', [-R*1.5 R*1.5 -R*1.5 R*1.5 -1 1], 'trail', {'r', 'LineWidth', 2}, 'top');
+robot.teach(q);
 
 %% CINEMÁTICA INVERSA
 fprintf("-----------DEFINICIÓN DEL PROBLEMA-------------\n");
 
-x = input('\nIndicar la coordenada X: ');
-y = input('Indicar la coordenada Y: ');
-fprintf("Problema --> [q1 q2] = f(%f, %f)\n", x, y);
-
-
-%% Fórmulas indicadas en la resolución del ejercicio
+x   = input('\nIndicar la coordenada X: ');
+y   = input('Indicar la coordenada Y: ');
+yaw = input('Indicar la orientacion - yaw(°): ');
+fprintf("Problema --> [q1 q2 q3] = f(%f, %f, %f°)\n", x, y, yaw);
 
 try
-    
-    fprintf("\n----------------SOUCIÓN GEOMÉTRICA--------------\n");
-    [q1 q2] = ikine2(robot, x, y);
+    [q1 q2 q3] = ikineRRR(robot, x, y, yaw);
     fprintf("\n------------------SOLUCIONES--------------------\n");
     fprintf("\nCodo abajo:");
-    fprintf("\nq = [q1 q2] = [%.2f° %.2f°]", rad2deg(q1(1)), rad2deg(q2(1)));
+    fprintf("\nq = [q1 q2 q3] = [%.2f° %.2f° %.2f°]", rad2deg(q1(1)), rad2deg(q2(1)), rad2deg(q3(1)));
     fprintf("\n\nCodo arriba:");
-    fprintf("\nq = [q1 q2] = [%.2f° %.2f°]", rad2deg(q1(2)), rad2deg(q2(2)));
+    fprintf("\nq = [q1 q2 q3] = [%.2f° %.2f° %.2f°]", rad2deg(q1(2)), rad2deg(q2(2)), rad2deg(q3(1)));
     fprintf("\n\n--------------VERIFICACIÓN CON RTB--------------\n");
 
     fprintf("Postura del robot codo abajo\n");
-    T = robot.fkine([q1(1) q2(1)]).T;
+    T = robot.fkine([q1(1) q2(1) q3(1)]).T;
     disp(T);
     fprintf("\nPosición del extremo\n");
     disp(T(1:2, 4));
     robotCU = SerialLink(robot, 'name', 'codo abajo');
     figure(2);
-    robotCU.plot([q1(1) q2(1)], 'workspace', [-(a1 + a2)*1.2 (a1 + a2)*1.2 -(a1 + a2)*1.2  (a1 + a2)*1.2 -1 1],'scale', 0.5,'trail', {'r', 'LineWidth', 2}, 'jointdiam', 0.5);
+    robotCU.plot([q1(1) q2(1) q3(1)], 'workspace', [-R*1.5 R*1.5 -R*1.5 R*1.5 -1 1],'scale', 0.5,'trail', {'r', 'LineWidth', 2}, 'jointdiam', 0.5);
 
     fprintf("\n------------------------------------------------\n");
     fprintf("Postura del robot codo arriba\n");
-    T       = robot.fkine([q1(2) q2(2)]).T;
+    T       = robot.fkine([q1(2) q2(2) q3(2)]).T;
     disp(T);
     fprintf("\nPosición del extremo\n");
     disp(T(1:2, 4));
     robotCD = SerialLink(robot, 'name','codo arriba');
     figure(3);
-    robotCD.plot([q1(2) q2(2)], 'workspace', [-(a1 + a2)*1.2 (a1 + a2)*1.2 -(a1 + a2)*1.2 (a1 + a2)*1.2 -1 1],'scale', 0.5,'trail', {'r', 'LineWidth', 2}, 'jointdiam', 0.5);
+    robotCD.plot([q1(2) q2(2) q3(2)], 'workspace', [-R*1.5 R*1.5 -R*1.5 R*1.5 -1 1],'scale', 0.5,'trail', {'r', 'LineWidth', 2}, 'jointdiam', 0.5);
     
 
-    fprintf("\n------------------------------------------------\n");
+    
 
     pause
     fprintf('\nPresione Enter para continuar.\n');
 catch ME
-    if (strcmp(ME.identifier, "MyException:ikine"))
+    if (strcmp(ME.identifier, "MyException:ikine2"))
         fprintf("\n\nExcepción en la cinemática inversa de ikine: ");
         disp(ME.message);
-    elseif (strcmp(ME.identifier, "MyException:ikine2"))
+    elseif (strcmp(ME.identifier, "MyException:ikineRRR"))
         fprintf("\n\nExcepción en la cinemática inversa de ikine1: ");
         disp(ME.message);
     else
@@ -83,33 +88,25 @@ catch ME
     end
 end
 
-%% CINEMÁTICA INVERSA MÉTODO GEOMÉTRICO PRIMERA APROXIMACIÓN
-% Se considera la formulación q = [q1 q2] = f(x, y)
-function [q1 q2] = ikine(robot, x, y)
+%% CINEMÁTICA INVERSA PARA EL ROBOT RRR
+function [q1 q2 q3] = ikineRRR(robot, x, y, gamma)
     if (~isa(robot, 'SerialLink'))
-        ME = MException("MyException:ikine", "el primer valor pasado a la función debe ser el robot.");
+        ME = MException("MyException:ikineRRR", "el primer valor pasado a la función debe ser el robot.");
         throw(ME);
     end
 
-    a1  = robot.a(1);               % longitud del primer eslabón
-    a2  = robot.a(2);               % longitud del segundo eslabon
+    % Primero se determina la posición del extremo del segundo eslabón
+    x_2 = x - robot.a(3)*cos(gamma);
+    y_2 = y - robot.a(3)*sin(gamma);
 
-    acosArg = (x^2 + y^2 - (a1^2 + a2^2))/(2*a1*a2); % argumento del acos
+    % Luego se resuleve el problema de la cinemática inversa del robot RR cono ikine2
+    [q1 q2] = ikine2(robot, x_2, y_2);
 
-    if (abs(acosArg) > 1)
-        ME = MException("MyException:ikine", "(x, y) fuera del alcance del robot");
-        throw(ME);
-    end
-
-    q2 = acos(acosArg);
-    A  = a1 + a2*cos(q2);
-    q2 = [q2; -q2];                   % Ambos valores de q2
-
-    B  = a2*sin(q2);
-    q1 = atan2(A*y - B*x, A*x + B*y); % Ambos valores de q1
+    % Finalmente se obtiene el valor de la tercera variable articular
+    q3 = gamma - q1 - q2;
 end
 
-%% CINEMÁTICA INVERSA MÉTODO GEOMÉTRICO PROPUESTO EN CLASE
+%% CINEMÁTICA INVERSA MÉTODO GEOMÉTRICO PROPUESTO EN CLASE PARA ROBOT EJERCICIO 1
 %{
     Se pretende hacer uso del mismo en otro Script con lo cual toma
     además al robot como parámetro.
@@ -169,6 +166,6 @@ function [q1 q2] = ikine2(robot, x, y)
 
     % calculo para la segunda alternativa
     T1      = robot.links(1).A(q1(2)).double;   % matriz de transformación directa en la primera articulación
-    p_1     = T1\[x; y; 0; 1];                  % coordenadas de p_o = (x, y, 0) respecto de 1, es decir p_1 = (x_1, y_1, 0)
+    p_1     = T1\[x; y; 0; 1] ;                 % coordenadas de p_o = (x, y, 0) respecto de 1, es decir p_1 = (x_1, y_1, 0)
     q2(2)   = atan2(p_1(2), p_1(1));
 end
