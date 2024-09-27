635eef241c7498d7e0ccd07c11a02c652cfc91c3
Juan <borquez.juan00@gmail.com>
2024-09-27 17:35:49 -0300

    path relativo modelo 3d

--- TP5/Ejercicios/ParteB/Robot/plot_teach_robot.m
+++ TP5/Ejercicios/ParteB/Robot/plot_teach_robot.m
@@ -1,9 +1,9 @@
-robot;
-p = fileparts(mfilename('fullpath'))
+robot;;
+%% plotear solamente el modelo cinemático
 %R.plot(q, 'workspace', workspace, 'scale', 0.5,'jointdiam', 0.85, 'trail', {'r', 'LineWidth', 0.1});
-
-% find the path to this specific model
-p = fullfile(p, '../../../../stls/UR10e')
-R.plot3d(q, 'path', p);
-
 %R.teach();
+
+%% Plotear con el modelo 3D
+p = fileparts(mfilename('fullpath'));
+p = fullfile(p, '../../../../stls/UR10e');
+R.plot3d(q, 'path', p);
