27946d43ecabb3a6be3c158be482083e8e3bd377
Juan <borquez.juan00@gmail.com>
2024-09-27 17:06:34 -0300

    plot3D con path relativo

--- TP5/Ejercicios/ParteB/Robot/plot_teach_robot.m
+++ TP5/Ejercicios/ParteB/Robot/plot_teach_robot.m
@@ -1,5 +1,9 @@
 robot;
-p = 'C:\Users\Public\Documents\UNCUYO\4-Cuarto_anio\2-Octavo_Semestre\6-Robotica I\3-TPS\TPS\stls\UR10e';
+p = fileparts(mfilename('fullpath'))
 %R.plot(q, 'workspace', workspace, 'scale', 0.5,'jointdiam', 0.85, 'trail', {'r', 'LineWidth', 0.1});
+
+% find the path to this specific model
+p = fullfile(p, '../../../../stls/UR10e')
 R.plot3d(q, 'path', p);
+
 %R.teach();
