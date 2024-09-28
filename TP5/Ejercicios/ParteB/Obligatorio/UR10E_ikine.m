799d71b78a94cfe4ade4c734def0f33431a373da
Juan <borquez.juan00@gmail.com>
2024-09-28 20:56:45 -0300

    aparentemente listorti

--- TP5/Ejercicios/ParteB/Obligatorio/UR10E_ikine.m
+++ TP5/Ejercicios/ParteB/Obligatorio/UR10E_ikine.m
@@ -1,104 +1,130 @@
-%function Q = UR10E_ikine(R, T, q0, q_mejor)
-function Q = UR10E_ikine(R, T)
-    % Verificacion que sea robot
+%function Q = UR10e_ikine(R, T, q0, q_mejor)
+function Q = UR10e_ikine(R, T)
+    %% Verificacion de parámetros
 
-    
-
-    if ~isa(R, 'SerialLink')
-        ME = MException("MyException:UR103_ikine", "el primer valor pasado a la función debe ser el robot.");
+    % Verificación del robot
+    if (~isa(R, 'SerialLink'))
+        ME = MException("MyException:UR10e_ikine", "el primer valor pasado a la función debe ser el robot.");
         throw(ME);
     end
-    % Deshacer R.tool y R.base
-    T        = R.base.double\T/R.tool.double;
 
-    % Rescatar los offsets
-    offsets  = R.offset;
-    R.offset = zeros(1, 6);
+    % Verificación de T
+    if (~isa(T, 'SE3'))
+        T  = SE3(T);
+    end
+    if (~T.ishomog())
+        ME = MException("MyException:UR10e_ikine", "T debe ser matriz de transformación homogénea");
+        throw(ME);
+    end
 
-    % Definición de las variables
-    d1 = R.d(1);
-    d2 = R.d(2);
-    d3 = R.d(3);
-    d4 = R.d(4);
-    d5 = R.d(5);
-    d6 = R.d(6);
+    %% Pasos previos a la cinemática inversa
+    %  Deshacer las operaciones de base y tool
+    T          = R.base.inv()*T*R.tool.inv();
 
-    a1 = R.a(1);
-    a2 = R.a(2);
-    a3 = R.a(3);
-    a6 = R.a(4);
-    a5 = R.a(5);
-    a6 = R.a(6);
+    % Definición de variables
+    d          =  R.d;
+    a          =  R.a;
 
-    n  = T(1:3, 1);
-    o  = T(1:3, 2);
-    a  = T(1:3, 3);
-    p  = T(1:3, 4);
+    nx         =  T.n(1);
+    ny         =  T.n(2);
+    nz         =  T.n(3);
 
-    nx = n(1);  % Componente x de n
-    ny = n(2);  % Componente y de n
-    nz = n(3);  % Componente z de n
-    
-    ox = o(1);  % Componente x de o
-    oy = o(2);  % Componente y de o
-    oz = o(3);  % Componente z de o
-    
-    ax = a(1);  % Componente x de a
-    ay = a(2);  % Componente y de a
-    az = a(3);  % Componente z de a
-    
-    px = p(1);  % Componente x de p
-    py = p(2);  % Componente y de p
-    pz = p(3);  % Componente z de p
+    ox         =  T.o(1);
+    oy         =  T.o(2);
+    oz         =  T.o(3);
 
-    % Calculo de q1
-    qq = zeros(6, 8);
+    ax         =  T.a(1);
+    ay         =  T.a(2);
+    az         =  T.a(3);
 
+    px         =  T.t(1);
+    py         =  T.t(2);
+    pz         =  T.t(3);
 
-    % Cálculo de q1
-    % falta verificar el discriminante de la raiz
-    q1(1) = atan2(d4,  sqrt((d6*ay - py)^2 + (px - d6*ax)^2 - d4^2)) - atan2(d6*ay - py, px - d6*ax);
-    q1(2) = atan2(d4, -sqrt((d6*ay - py)^2 + (px - d6*ax)^2 - d4^2)) - atan2(d6*ay - py, px - d6*ax);
+    %  Rescatar los offsets
+    offsets    = R.offset;
+    R.offset   = zeros(1, 6);
 
-    qq(1,1:4) = q1(1);
-    qq(1,5:8) = q1(2);
+    %%  Cálculo de q1
+
+    %  Verificación del discriminante de la raiz
+    disc = (d(6)*ay - py)^2 + (px - d(6)*ax)^2 - d(4)^2;
+
+    if (disc < 0)
+        warning('UR10e_ikine: q1 complejo', 'El punto está fuera del alcance de robot. Conservando la parte real...');
+    end
+
+    phi   = atan2(d(6)*ay - py, px - d(6)*ax);
+    q1(1) = real(atan2(d(4),  sqrt(disc))) - phi;
+    q1(2) = real(atan2(d(4), -sqrt(disc))) - phi;
 
     % S1 Y C1
-    S1(1:4,1) = sin(qq(1,1:4));
-    S1(5:8,1) = sin(qq(1,5:8));
-    C1(1:4,1) = cos(qq(1,1:4));
-    C1(5:8,1) = cos(qq(1,5:8));
+    S1    = sin(q1);
+    C1    = cos(q1);
     
-    % Cálculo de q5
+    %% Cálculo de q5
     q5(1:2) = atan2(sqrt((nx*S1 - ny*C1).^2 + (ox*S1 - oy*C1).^2), ax*S1 - ay*C1);
-    q5(3:4) = atan2(-sqrt((nx*S1 - ny*C1).^2 + (ox*S1 - oy*C1).^2), ax*S1 - ay*C1);
+    q5(3:4) = -q5(1:2);
 
-    qq(5,[1 2 5 6]) = q5(1);
-    qq(5,[3 4 7 8]) = q5(3);
+    % Actualizaicón de q1
+    q1(3:4) = q1(1:2);
 
-    % Cálculo de q6
-    S5 = sin(qq(1,1:8));
+    %% Actualización de senos y cosenos
+    S1    = sin(q1);
+    C1    = cos(q1);
+    S5    = sin(q5);
 
-    % Cálculo de q6
-    % Tomar en cuenta de que para 0 y npi se tiene singularidad
-    q6 = atan2((-ox*S1 - oy*C1) ./ S5, (nx*S1 - ny*C1) ./ S5);
-    qq(5,[1 2 5 6]) = q5(1);
-    qq(5,[3 4 7 8]) = q5(3);
+    %% Cálculo de q6
+    if any(S5 == 0)        % Para 0 y npi se tiene singularidad
+        warning('UR10e_ikine: q5 == 0 o n*pi', 'Singularidad');
+        % En este caso q6 y q234defaults to 45°
+    end
+    q6    = atan2((-ox*S1 - oy*C1) ./ S5, (nx*S1 - ny*C1) ./ S5);
+    
+    %% Cálculo de q2
+    q234  = atan2((-az ./ S5), (-ax*C1 + ay*S1) ./ S5);
 
-    % Cálculo de q2
-    % Tomar en cuenta de que para 0 y npi se tiene singularidad
-    q234 = atan2((-az ./ S5), (-ax*C1 + ay*S1) ./ S5);
+    S234  = sin(q234);
+    C234  = cos(q234);
 
-    S234 = sin(q234);
-    C234 = cos(q234);
+    A     = px*C1 + py*S1 - d(5)*S234 + d(6)*S5.*C234;
+    B     = pz - d(2) + d(5)*C234 + d(6)*S5.*S234;
 
-    A    = px*C1 + py*S1 - d5*S234 + d6*S5.*C234;
-    B    = pz - d1 + d5*C234 + d6*S5.*S234;
 
-    num  = (A.^2 + B.^2 + a2^2 - a3^2);
-    den = sqrt(4*a2^2*a3^2 - (A.^2 + B.^2 - a2^2 - a3^2)^2);
+    %  Verificación del discriminante de la raiz
+    disc = 4*a(2)^2*a(3)^2 - (A.^2 + B.^2 - a(2)^2 - a(3)^2).^2;
 
-    q2 = atan2(num, den) - atan2(A, B);
-    % Cálculo de q3
-    Q=1;
+    if (any(disc) < 0)
+        warning('UR10e_ikine: q2 complejo', 'El punto está fuera del alcance de robot. Conservando la parte real...');
+    end
+
+    num     = A.^2 + B.^2 + a(2)^2 - a(3)^2;
+    den     = sqrt(disc);
+
+    q2(1:4) = real(atan2(num,  den)) - atan2(A, B);
+    q2(5:8) = real(atan2(num, -den)) - atan2(A, B);
+    C2      = cos(q2);
+    S2      = sin(q2);
+
+    %% Cálculo de q3
+    C23(1:4) = (A -  a(2)*C2(1:4))/a(3);
+    C23(5:8) = (A -  a(2)*C2(5:8))/a(3);
+    S23(1:4) = (B -  a(2)*S2(1:4))/a(3);
+    S23(5:8) = (B -  a(2)*S2(5:8))/a(3);
+
+    q23      = atan2(S23, C23);
+    q3       = q23 - q2;
+
+    %% Cálculo de q4
+    %  Actualización de variables
+    q234(5:8)   = q234(1:4);
+
+    q1(5:8)     = q1(1:4);
+    q5(5:8)     = q5(1:4);
+    q6(5:8)     = q6(1:4);
+    q4          = q234 - q23;
+
+    %% Solucion final
+    qq = [q1; q2; q3; q4; q5; q6];
+    Q  = qq;
 end
--- TP5/Ejercicios/ParteB/Obligatorio/UR10E_ikine_test.m
+++ TP5/Ejercicios/ParteB/Obligatorio/UR10E_ikine_test.m
@@ -1,7 +1,8 @@
-run("robot.m")
+robotFile = fullfile(fileparts(mfilename('fullpath')), '../../../../robot/robot');
+run(robotFile);
 T = [0.866 -0.500 0.000 0.800;
      0.500  0.866 0.000 0.300;
      0.000  0.000 1.000 0.400;
      0.000  0.000 0.000 1.000];
 
-Q = UR10E_ikine(R,T);
+Q = UR10e_ikine(R,T);
