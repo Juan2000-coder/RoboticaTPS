ff8d129a8c2f3d996e78a92a72206ea3236b34f5
Lianju0 <julian.rayes0911@gmail.com>
2024-09-28 14:00:15 -0300

    carpeta de robot con codigo corregido

---
+++ robot/plot_teach_robot.m
@@ -0,0 +1,9 @@
+robot;
+%% plotear solamente el modelo cinemático
+R.plot(q, 'workspace', workspace, 'scale', 0.5,'jointdiam', 0.85, 'trail', {'r', 'LineWidth', 0.1});
+R.teach();
+
+%% Plotear con el modelo 3D
+%p = fileparts(mfilename('fullpath'));
+%p = fullfile(p, '../../../../stls/UR10e');
+%R.plot3d(q, 'path', p);
