6eaab5c454cbd60f54aa6641661312af08dffa72
Juan <borquez.juan00@gmail.com>
2024-10-10 10:59:29 -0300

    tratando de plot3d

--- robot/plot_teach_robot.m
+++ robot/plot_teach_robot.m
@@ -1,9 +1,12 @@
 robot;
 %% plotear solamente el modelo cinemático
-R.plot(q, 'workspace', workspace, 'scale', 0.5,'jointdiam', 0.85, 'trail', {'r', 'LineWidth', 0.1});
-R.teach();
+R.plot(q,'scale', 0.5,'jointdiam', 0.85, 'trail', {'r', 'LineWidth', 0.1});
+%R.teach();
 
 %% Plotear con el modelo 3D
-%p = fileparts(mfilename('fullpath'));
-%p = fullfile(p, '../../../../stls/UR10e');
-%R.plot3d(q, 'path', p);
+p = fileparts(mfilename('fullpath'));
+R.plot3d(q, 'path', p);
+
+%q = fileparts(mfilename('fullpath'));
+%p = fullfile(q, '../stls/UR10e');
+%demmam(p, q);
