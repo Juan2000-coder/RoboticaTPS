39bcbdcae45f8da2df5a600c7ad1209125ece35e
Juan <borquez.juan00@gmail.com>
2024-10-21 17:54:14 -0300

    Ejercicio 1 TP8

---
+++ TP8/Ejercicios/Ejercicio_1.m
@@ -0,0 +1,29 @@
+Fanuc       % Definición del robot FANUC
+fprintf('######################################################\n');
+fprintf('#                Ejercicios 1  TP8                   #\n');
+fprintf('######################################################\n\n');
+pause;
+
+%% Inciso 1
+q0  = [0     -pi/2     0    0    0 0];  % Posición inicial
+q1  = [-pi/3 pi/10 -pi/5 pi/2 pi/4 0];  % Posición final
+ti  = 0;                                % Instante inicial
+tf  = 3;                                % Instante final
+dt  = 0.1;                              % Paso de tiempo
+t   = ti:dt:tf;                          % 3 seg. y una decima de paso
+Q   = jtraj(q0, q1, t);                 % Trayectoria interpolada
+
+fprintf("\nInterpolación entre puntos articulares:\n");
+fprintf("\nq0: \n");disp(q0);
+fprintf("\nq1: \n");disp(q1);
+fprintf("\nTrayectoria interpolada: \n"); disp(Q);
+fprintf("\nDimensiones de la matriz con posiciones interpladas: \n"); disp(size(Q));
+
+%% Inciso 2
+figure(1);
+fanuc.plot([0 0 0 0 0 0], 0.65,'jointdiam', 0.65, 'trail', {'r', 'LineWidth', 0.1});
+fprintf("\nPresione ENTER para visualizar la animación del robot.\n");
+for q = Q
+    fanuc.animate(q);
+    pause(dt);
+end
---
+++ TP8/Ejercicios/Fanuc.m
@@ -0,0 +1,29 @@
+close all;
+clear;
+clc;
+
+% Parametros DH robot
+%% Parámetros DH Fanuc
+dh = [0 0.450 0.075 -pi/2 0;
+      0 0.000 0.300  0.0  0;
+      0 0.000 0.075 -pi/2 0;
+      0 0.320 0.000  pi/2 0;
+      0 0.000 0.000 -pi/2 0;
+      0 0.008 0.000  0.0  0];
+
+% Variables articulares
+q  = [0 0 0 0 0 0];
+
+% Construcción del robot
+fanuc      = SerialLink(DH, 'name', 'FANUC Paint Mate 200iA');
+
+% Offsets iniciales
+fanuc.offset = [0.0 pi/2 0.0 0.0 0.0 0.0];
+
+% Límites obtenidos del datasheet del robot
+fanuc.qlim   = deg2rad([-170   170;
+                        -100   100;
+                        -194   194;
+                        -190   190;
+                        -120   120;
+                        -360   360]);
