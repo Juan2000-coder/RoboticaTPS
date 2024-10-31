24664a3835eb74250bfd49da0c0120a62e0cea5f
Juan <borquez.juan00@gmail.com>
2024-10-31 07:59:30 -0300

    mov archivos

--- robot/UR10E_ikine.m
+++ robot/UR10E_ikine.m
@@ -1,7 +1,4 @@
 function qq = UR10E_ikine(R, T, q0, mejor)
-    %% Devuelve qq(1 x 6) cuando mejor =  true
-    %% Devuelve qq(8 x 6) cuando mejor = false
-    
     %% Verificacion de parámetros
 
     % Verificación del robot
@@ -144,5 +141,5 @@ function qq = UR10E_ikine(R, T, q0, mejor)
         [~,pos] = min(normas);
         qq = qq(:, pos);
     end
-    qq = transpose(qq);
+
 end
