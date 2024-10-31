6933b86df7e217c9f8bdddfbe685f1d682ad3fae
Juan <borquez.juan00@gmail.com>
2024-10-31 18:36:03 -0300

    codigos del final

---
+++ Trabajo Final/Codigos/robot/frames.m
@@ -0,0 +1,50 @@
+clc; clear; close all;
+
+% llamado a robot.m para que cargue el archivo, que esta en el mismo directorio
+robot; % crea el objeto R
+
+% asegurarse de que R es un objeto SerialLink antes de proceder
+if ~exist('R', 'var') || ~isa(R, 'SerialLink')
+    error('El objeto R no se ha cargado correctamente como un objeto SerialLink.');
+end
+
+% definicion del vector de coordenadas articulares
+q_analysis = [0.5, -0.5, 0.5, -0.5, 0.5, -0.5]; 
+
+% definicion booleana para decidir que sistemas se grafican
+sistemas = [1, 1, 1, 1, 1, 1, 1];
+
+% definir colores alternados: rojo ('r') y azul ('b')
+colors = ['r', 'b'];  % Alternar entre rojo y azul
+
+% ploteo del robot
+figure;
+R.plot(q_analysis, 'scale', 0.5, 'jointdiam', 0.85, 'notiles');
+hold on;
+
+% graficar el sistema de referencia base S0
+if sistemas(1)
+    trplot(R.base, 'frame', '0', 'color', 'r', 'length', 0.5, 'thick', 1);
+end
+
+for i = 1:length(R.links)
+    if sistemas(i + 1)
+        T = R.A(1:i, q_analysis);  % calcula la transformación acumulada hasta el eslabón i
+        T = (R.base.T)*(T.T);      % extraer la matriz homogénea de la transformación
+
+        if i == length(R.links)
+            T = T*(R.tool.T);
+        end
+
+        % alternar colores: si el índice es par, usa azul ('b'); si es impar, usa rojo ('r')
+        color = colors(mod(i, 2) + 1);
+
+        % graficar el sistema de referencia correspondiente con el color alternado
+        trplot(T, 'frame', num2str(i), 'color', color, 'length', 0.5, 'thick', 1);
+    end
+end
+
+% ajustar los límites de los ejes según el espacio de trabajo
+axis(workspace);
+
+hold off;
