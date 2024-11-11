587f466cd9e44deb4e8cb671abbccf1ae27a2096
Juan <borquez.juan00@gmail.com>
2024-11-11 10:17:36 -0300

    arregleo de q234_singu

--- robot/q234_singu.m
+++ robot/q234_singu.m
@@ -1,4 +1,4 @@
-function [qsol1, qsol2] = q234_singu(R, varargin)
+function q = q234_singu(R, varargin)
     % Verificación del robot
     if (~isa(R, 'SerialLink'))
         ME = MException("miTeach:isSerialLink", "El primer valor pasado a la función debe ser el robot.");
@@ -23,6 +23,7 @@ function [qsol1, qsol2] = q234_singu(R, varargin)
     q3           = p.Results.q3;
     q4           = p.Results.q4;
 
+    q = [];
     if islogical(q2)
         % Definir A y B de acuerdo con las formulas
         A = cos(2*q3) - Ca*cos(q4) + Ca*cos(2*q3 + q4) - 1;
@@ -31,40 +32,45 @@ function [qsol1, qsol2] = q234_singu(R, varargin)
         % Calcular q2 usando la función atan2
         q2_1 = atan2(B, A*Sb) + pi/2;
         q2_2 = q2_1 - pi;
-        qsol1 = q2_1;
-        qsol2 = q2_2;
+        q    = [q2_1, q2_2];
     elseif islogical(q4)
         m       = -1/tan(q2);
         a       = Sg*sin(q3)      + m*Sb*Ca*cos(q3);
         b       = m*Sb*Ca*sin(q3) - Sg*cos(q3);
         c       = 1 + Sb*cos(q3)  - m*Sb*sin(q3);
-        a_cuad  = a^2 + b ^ 2;
+        a_cuad  = a^2 + b^2;
         b_cuad  = -2*b*c;
         c_cuad  = c^2 - a^2;
         disc    = b_cuad^2 - 4*a_cuad*c_cuad;
         if (disc < 0)
-            q4_1 = [];
-            q4_2 = [];
             warning('q234singu:q4', 'No hay solución real para q4');
         else
             c4_1 = (-b_cuad + sqrt(disc)) / (2*a_cuad);
             c4_2 = (-b_cuad - sqrt(disc)) / (2*a_cuad);
+
             % obtener c4_i con valor absoluto menor a 1
             if (abs(c4_1) <= 1)
-                q4_1 = atan2(sqrt(1 - c4_1^2), c4_1);
-                q4_2 = atan2(-sqrt(1 - c4_1^2), c4_1);
-            elseif (abs(c4_2) <= 1)
-                q4_1 = atan2(sqrt(1 - c4_2^2), c4_2);
-                q4_2 = atan2(-sqrt(1 - c4_2^2), c4_2);
-            else
-                q4_1 = [];
-                q4_2 = [];
+                s4_1 = sqrt(1 - c4_1^2);
+                if (abs(a*s4_1 + b*c4_1 - c) < eps)
+                    q4_1 = atan2(s4_1, c4_1);
+                else
+                    q4_1 = atan2(-s4_1, c4_1);
+                end
+                q = [q4_1];
+            end
+            if (abs(c4_2) <= 1)
+                s4_2 = sqrt(1 - c4_2^2);
+                if (abs(a*s4_2 + b*c4_2 - c) < eps)
+                    q4_2 = atan2(s4_2, c4_2);
+                else
+                    q4_2 = atan2(-s4_2, c4_2);
+                end
+                q = [q, q4_2];
+            end
+            if (abs(c4_1) > 1 && abs(c4_2) > 1)
                 warning('q234singu:q4', 'No hay solución real para q4');
             end
         end
-        qsol1 = q4_1;
-        qsol2 = q4_2;
-
     elseif islogical(q3)
         m       = -1/tan(q2);
 
@@ -73,30 +79,39 @@ function [qsol1, qsol2] = q234_singu(R, varargin)
         b = m*Sb*Ca*sin(q4) - Sg*cos(q4) - Sb;
         c = 1;
 
-        a_cuad  = a^2 + b ^ 2;
+        a_cuad  = a^2 + b^2;
         b_cuad  = -2*b*c;
         c_cuad  = c^2 - a^2;
 
         % Calcular el discriminante
-        discriminante = b_cuad^2 - 4*a_cuad*c_cuad;
-
-        % Calcular las soluciones de la ecuación cuadrática
-        c3_1 = (-b_cuad + sqrt(discriminante)) / (2*a_cuad);
-        c3_2 = (-b_cuad - sqrt(discriminante)) / (2*a_cuad);
-
-        % obtener c3_i con valor absoluto menor a 1
-        if (abs(c3_1) <= 1)
-            q3_1 = atan2(sqrt(1 - c3_1^2), c3_1);
-            q3_2 = atan2(-sqrt(1 - c3_1^2), c3_1);
-        elseif (abs(c3_2) <= 1)
-            q3_1 = atan2(sqrt(1 - c3_2^2), c3_2);
-            q3_2 = atan2(-sqrt(1 - c3_2^2), c3_2);
-        else
-            q3_1 = [];
-            q3_2 = [];
+        disc = b_cuad^2 - 4*a_cuad*c_cuad;
+        if (disc < 0)
             warning('q234singu:q3', 'No hay solución real para q3');
+        else
+            % Calcular las soluciones de la ecuación cuadrática
+            c3_1 = (-b_cuad + sqrt(disc)) / (2*a_cuad);
+            c3_2 = (-b_cuad - sqrt(disc)) / (2*a_cuad);
+
+            % obtener c3_i con valor absoluto menor a 1
+            if (abs(c3_1) <= 1)
+                s3_1 = sqrt(1 - c3_1^2);
+                if (abs(a*s3_1 + b*c3_1 - c) < eps)
+                    q3_1 = atan2(s3_1, c3_1);
+                else
+                    q3_1 = atan2(-s3_1, c3_1);
+                end
+                q = [q3_1];
+            if (abs(c3_2) <= 1)
+                s3_2 = sqrt(1 - c3_2^2);
+                if (abs(a*s3_2 + b*c3_2 - c) < eps)
+                    q3_2 = atan2(s3_2, c3_2);
+                else
+                    q3_2 = atan2(-s3_2, c3_2);
+                end
+                q = [q, q3_2];
+            else
+                warning('q234singu:q3', 'No hay solución real para q3');
+            end
         end
-        qsol1 = q3_1;
-        qsol2 = q3_2;
     end
 end
